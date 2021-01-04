49f171b74e9c9398043190e33dc616c5b4d14dfe
nori3 <62231108+norizoh@users.noreply.github.com>
2021-01-04 23:32:17 +0900

    map, filterの確認

--- src/index.js
+++ src/index.js
@@ -128,11 +128,40 @@
 // console.log(arr3);
 
 //配列のコピー、結合
-const arr4 = [10, 20];
-const arr5 = [30, 40];
+// const arr4 = [10, 20];
+// const arr5 = [30, 40];
 
-const arr6 = [...arr4];
-console.log(arr6);
+// const arr6 = [...arr4];
+// console.log(arr6);
 
-const arr7 = [...arr4, ...arr5];
-console.log(arr7);
+// const arr7 = [...arr4, ...arr5];
+// console.log(arr7);
+
+//map,filter を使った配列の処理
+//従来
+const nameArr = ["田中", "やまだ", "じゃけぇ"];
+// for (let index = 0; index < nameArr.length; index++){
+//   console.log(`${index + 1}番目は、${nameArr[index]}です。`);
+// }
+
+// const nameArr2 = nameArr.map((name) => {
+//   return name;
+// } )
+// console.log(nameArr2);
+
+//nameArr.map((name, index) => console.log(`${index}番目は${name}です。`));
+
+// const numArr = [1,2,3,4,5];
+// const newNumArr = numArr.filter((num) => {
+//   return num % 2 === 1;
+// });
+// console.log(newNumArr);
+
+const newNameArr = nameArr.map((name) => {
+  if (name === "じゃけぇ") {
+    return name;
+  } else {
+    return `${name}さん`;
+  }
+});
+console.log(newNameArr);
